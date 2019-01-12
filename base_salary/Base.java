7cd0e5ad21124a0ab70d7b606222d62343f8e88c
spencer <snixo92@gmail.com>
2019-01-12 13:28:03 -0500

    Initial commit

---
+++ base_salary/Base.java
@@ -0,0 +1,24 @@
+package base_salary;
+
+public class Base {
+	
+	
+	public static double computeIncome(double salesAmount) {
+		System.out.println(salesAmount);
+		return salesAmount;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		computeIncome(1000);
+		
+		
+		System.out.println("Hello World!");
+
+	}
+	
+	
+
+}
