0cd0c315303e26a3a3e3486889ffa57a3ae74389
spencer <snixo92@gmail.com>
2019-01-13 18:28:15 -0500

    Finished separating logic into function/cleanup work

--- base_salary/Base.java
+++ base_salary/Base.java
@@ -2,41 +2,47 @@ package base_salary;
 
 public class Base {
 	
-	// Do calculations in here instead of printing value
 	public static double computeIncome(double salesAmount) {
-		System.out.println("         " + "$" + salesAmount);
-		return salesAmount;
+		
+		double maximumAmount = 0;
+		double maxFromPreviousTier = 0;
+		double maxSecondTier = 0;
+		double incomeAmount = 0.00;
+		
+		if (salesAmount < 5001) {	
+			incomeAmount = (salesAmount * 0.08) + 5000;
+		}
+		
+		else if (salesAmount < 10001) {
+			maximumAmount = 5000;
+			maxFromPreviousTier = 5000;
+			incomeAmount = (maxFromPreviousTier * 0.08) + ((salesAmount - maximumAmount) * 0.1) + 5000;
+		}
+		
+		else if (salesAmount < 20001) {
+			maximumAmount = 10000;
+			maxFromPreviousTier = 5000;
+			maxSecondTier = 5000;
+			incomeAmount = (maxFromPreviousTier * 0.08) + (maxSecondTier * 0.1) + ((salesAmount - maximumAmount) * 0.12) + 5000;
+		}
+		
+		return incomeAmount;
 	}
 	
 	public static void main(String[] args) {
 
-		double maximumAmount = 0;
-		double maxFromPreviousTier = 0;
-		double maxSecondTier = 0;
-		double sales = 1000;
+		
+		double sales = 1000.00;
 		
 		System.out.println("Sales" + "           " + "Income");
 		
 		for (int i = 0; i < 20; i++) {
 			
-			System.out.print("$" + sales);
+			System.out.format("$%.2f",sales);
+			double result = computeIncome(sales);
+			System.out.format("        $%.2f", result);
 			
-			if (sales < 5001) {	
-				computeIncome((sales * 0.08) + 5000);
-			}
-			
-			else if (sales < 10001) {
-				maximumAmount = 5000;
-				maxFromPreviousTier = 5000;
-				computeIncome( (maxFromPreviousTier * 0.08) + ((sales - maximumAmount) * 0.1) + 5000);
-			}
-			
-			else if (sales < 20001) {
-				maximumAmount = 10000;
-				maxFromPreviousTier = 5000;
-				maxSecondTier = 5000;
-				computeIncome( (maxFromPreviousTier * 0.08) + (maxSecondTier * 0.1) + ((sales - maximumAmount) * 0.12) + 5000);
-			}
+			System.out.println("");
 			
 			sales += 1000;
 			
